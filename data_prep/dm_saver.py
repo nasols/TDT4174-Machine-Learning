8a363530e4cbf4ffb3b67d76c7dc0db82bf3e612
HenrikLogJacobsen <loghenrik@gmail.com>
2023-10-13 18:39:06 +0200

    dm-saver

---
+++ data_prep/dm_saver.py
@@ -0,0 +1,36 @@
+import pickle
+
+class DM_Saver() : 
+
+    def __init__(self) : 
+
+        self.data = {}
+
+    def set_data(self, dm): 
+        self.data = {
+            'train_a': dm.train_a,
+            'train_b': dm.train_b,
+            'train_c': dm.train_c,
+            'X_train_estimated_a': dm.X_train_estimated_a,
+            'X_train_estimated_b': dm.X_train_estimated_b,
+            'X_train_estimated_c': dm.X_train_estimated_c,
+            'X_train_observed_a': dm.X_train_observed_a,
+            'X_train_observed_b': dm.X_train_observed_b,
+            'X_train_observed_c': dm.X_train_observed_c,
+            'X_test_estimated_a': dm.X_test_estimated_a,
+            'X_test_estimated_b': dm.X_test_estimated_b,
+            'X_test_estimated_c': dm.X_test_estimated_c,
+            'amplitude': dm.amplitude
+        }
+
+    def save(self, filename):
+        # Serialize and save the Data_Manager instance to a file
+        with open(filename, 'wb') as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def load(cls, filename):
+        # Load a serialized Data_Manager instance from a file
+        with open(filename, 'rb') as file:
+            data_manager = pickle.load(file)
+        return data_manager
